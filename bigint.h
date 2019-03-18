23edc2360f54d840eb926f1413d9d627337c998c
xuweizhe <48399095+bluecloudi@users.noreply.github.com>
2019-03-18 13:08:46 +0800

    Update bigint.h

--- bigint.h
+++ bigint.h
@@ -8,17 +8,28 @@ namespace std
 		int len,f;
 		BigInt()
 		{
-			len=0;
-			f=1;
+			len=f=1;
+			num.clear();
 		}
-		friend ostream& operator <<(ostream& out,const BigInt& val)
+		int& operator [](int index)
 		{
-			if(val.f==-1) out<<'-';
-			for(int i=val.len;i>=1;i--)
+			return num[index];
+		}
+		void operator =(BigInt b)
+		{
+			len=b.len;
+			f=b.f;
+			num=b.num;
+		}
+		void operator =(string s)
+		{
+			if(s[0]=='-') f=-1,len=s.size()-1;
+			else f=1,len=s.size();
+			int c=s.size();
+			for(int i=1;i<=len;i++)
 			{
-				out<<val.num[i];
+				num[i]=s[c-i]-'0';
 			}
-			return out;
 		}
 		friend istream& operator >>(istream& in,BigInt &val)
 		{
@@ -27,155 +38,124 @@ namespace std
 			val=s;
 			return in;
 		}
-		bool operator >(const BigInt &b)
+		friend ostream& operator <<(ostream& out,const BigInt &val)
 		{
-			if(len>b.len) return true;
-			if(len<b.len) return false;	
-			for(int i=len;i>=1;i--)
+			if(val.f==-1) out<<'-';
+			for(int i=val.len;i>=1;i--)
 			{
-				if(num[i]>b.num[i]) return true;
-				if(num[i]<b.num[i]) return false;
+				out<<val.num[i];
 			}
-			return false;
+			return out;
 		}
-		bool operator <(const BigInt &b)
+		friend BigInt operator +(BigInt a,BigInt b)
 		{
-			if(len<b.len) return true;
-			if(len>b.len) return false;	
-			for(int i=len;i>=1;i--)
+			if(a.f==-1&&b.f==1)
 			{
-				if(num[i]<b.num[i]) return true;
-				if(num[i]>b.num[i]) return false;
+				a.f=1;
+				return b-a;
 			}
-			return false;
-		}
-		void operator =(const BigInt &b)
-		{
-			len=b.len;
-			f=b.f;
-			this->num=b.num;
-		}
-		BigInt operator +(BigInt b)
-		{
-			BigInt c;
-			if(b.f==-1&&f==1)
+			if(a.f==1&&b.f==-1)
 			{
 				b.f=1;
-				return *this-b;
+				return a-b;
 			}
-			if(f==-1&&b.f==1)
-			{
-				c=*this;
-				c.f=1;
-				return b-c;
-			}   
-			if(f==1) c.f=1;
+			BigInt c;
+			if(a.f==1) c.f=1;
 			else c.f=-1;
-			c.len=max(len,b.len);
-			int x=0;
-			for(int i=1;i<=c.len;i++)
+			c.len=max(a.len,b.len);
+			int i,x=0;
+			for(i=1;i<=c.len;i++)
 			{
-				c.num[i]=num[i]+b.num[i]+x;
-				x=c.num[i]/10;
-				c.num[i]%=10;
+				c[i]=a[i]+b[i]+x;
+				x=c[i]/10;
+				c[i]%=10;
 			}
-			if(x!=0) c.num[++c.len]=x;
+			if(x) c[++c.len]=x;
 			return c;
 		}
-		void operator =(const string &s)
+		friend BigInt operator -(BigInt a,BigInt b)
 		{
-			if(s[0]=='-')
+			if(a.f==-1&&b.f==1)
 			{
-				f=-1;
-				len=s.size()-1;
-			} 
-			else
-			{
-				f=1;
-				len=s.size();
-			} 
-			num.clear();
-			num.push_back(0);
-			for(int i=1;i<=len;i++)
-			{
-				num.push_back(s[s.size()-i]-'0');
+				b.f=-1;
+				return a+b;
 			}
-		}
-		BigInt operator -(BigInt b)
-		{
-			BigInt c;
-			if(f==1&&b.f==-1)
+			if(a.f==1&&b.f==-1)
 			{
 				b.f=1;
-				return *this+b;
+				return a+b;
 			}
-			if(f==-1&&b.f==1)
+			if(a.f==-1&&b.f==-1)
 			{
-				c=*this;
-				b.f=-1;
-				return c+b;
+				a.f=1;
+				b.f=1;
+				return b-a;
 			}
-			if(*this<b)
+			BigInt c;
+			if(a<b)
 			{
 				c.f=-1;
 				c.len=b.len;
 				for(int i=1;i<=b.len;i--)
 				{
-					c.num[i]=b.num[i]-num[i];
-					if(c.num[i]<0)
-					{
-						b.num[i+1]--;
-						c.num[i]+=10;
-					} 
+					c[i]=b[i]-a[i];
+					if(c[i]<0) c[i]+=10,b[i+1]--;
 				}
+				while(c[c.len]==0&&c.len>1) c.len--;
 			}
 			else
 			{
 				c.f=1;
-				c.len=len;
-				for(int i=1;i<=len;i--)
+				c.len=a.len;
+				for(int i=1;i<=a.len;i--)
 				{
-					c.num[i]=num[i]-b.num[i];
-					if(c.num[i]<0)
-					{
-						num[i+1]--;
-						c.num[i]+=10;
-					} 
+					c[i]=a[i]-b[i];
+					if(c[i]<0) c[i]+=10,a[i+1]--;
 				}
+				while(c[c.len]==0&&c.len>1) c.len--;
 			}
 			return c;
 		}
-		BigInt operator *(const BigInt &b)
+		friend bool operator <(BigInt a,BigInt b)
 		{
-			BigInt c;
-			if(f==-1&&b.f==-1) c.f=1;
-			if(f==1&&b.f==1) c.f=1;
-			if(f==1&&b.f==-1) c.f=-1;
-			if(f==-1&&b.f==1) c.f=-1;
-			c.len=len+b.len-1;
-			int x=0,i,j;
-			for(i=1;i<=len;i++)
+			if(a.len<b.len) return true;
+			if(a.len>b.len) return false;	
+			for(int i=a.len;i>=1;i--)
 			{
-				for(j=1;j<=b.len;j++)
-				{
-					c.num[i+j-1]+=num[i]*b.num[j]+x;
-					x=c.num[i+j-1]/10;
-					c.num[i+j-1]%=10;
-				}
-				while(x)
-				{
-					if(i+b.len<=c.len) c.num[i+b.len]=x%10;
-					else c.num[++c.len]=x%10;
-					x/=10;
-				}
+				if(a[i]<b[i]) return true;
+				if(a[i]>b[i]) return false;
 			}
+			return false;
+		}
+		friend bool operator >(BigInt a,BigInt b)
+		{
+			return b<a;
+		}
+		friend bool operator !=(BigInt a,BigInt b)
+		{
+			return b<a||a>b;
+		}
+		friend bool operator ==(BigInt a,BigInt b)
+		{
+			return !(a!=b);
+		}
+		friend bool operator >=(BigInt a,BigInt b)
+		{
+			return !(a<b);
+		}
+		friend bool operator <=(BigInt a,BigInt b)
+		{
+			return !(a>b);
+		}
+		friend BigInt operator +=(BigInt a,const BigInt &b)
+		{
+			BigInt c=a+b;
 			return c;
- 		}
- 	 	BigInt abs(void)
- 	 	{
- 	 		BigInt c=*this;
- 	 		c.f=1;
- 	 		return c;
+		}
+		friend BigInt operator -=(BigInt a,const BigInt &b)
+		{
+			BigInt c=a-b;
+			return c;
 		}
 	};
 	#define bigint BigInt
