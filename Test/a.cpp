c2fc22a42007fc724b8299cb21d5fb07f1f1a420
xuweizhe <48399095+bluecloudi@users.noreply.github.com>
2019-03-23 21:15:27 +0800

    Update a.cpp

--- Test/a.cpp
+++ Test/a.cpp
@@ -1,1 +1,14 @@
-
+#include<bigint.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	bigint a,b;
+	cin>>a>>b;
+	cout<<a+b<<endl;
+	cout<<a-b<<endl;
+	cout<<a*b<<endl;
+	cout<<a/b<<endl;
+	cout<<a%b<<endl;
+	return 0;
+}
